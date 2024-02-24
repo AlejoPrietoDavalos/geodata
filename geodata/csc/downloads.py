677bfd26d3b8384ea69aae50675cae4440c526e1
AlejoPrietoDavalos <alejoprietodavalos@gmail.com>
2024-02-24 13:35:21 -0300

    Replace None in df downloaded

--- geodata/csc/downloads.py
+++ geodata/csc/downloads.py
@@ -5,6 +5,9 @@ import pandas as pd
 
 from geodata.csc.urls import UrlsCSC
 
+def replace_empty_to_none(x):
+    return None if isinstance(x, str) and x.strip() == '' else x
+
 def download_csv(url: str | UrlsCSC, sep=",") -> pd.DataFrame:
     """ Download the raw data from the CSC github.
     - Returns a dataframe with the csv.
@@ -12,4 +15,6 @@ def download_csv(url: str | UrlsCSC, sep=",") -> pd.DataFrame:
     url = UrlsCSC(url).value
     response = requests.get(url)
     df = pd.read_csv(StringIO(response.text), sep=sep)
+    df = df.where(pd.notnull(df), None)
+    df = df.map(replace_empty_to_none)
     return df
