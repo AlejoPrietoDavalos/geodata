4b28ff28013c1cd7df7f52cfa80fb1ee4b8e45b8
AlejoPrietoDavalos <alejoprietodavalos@gmail.com>
2024-03-04 08:59:40 -0300

    download_csc

--- geodata/db/client.py
+++ geodata/db/client.py
@@ -2,11 +2,11 @@ from typing import Dict, List
 from abc import ABC
 from functools import cached_property
 
-from pymongo import MongoClient, DESCENDING, ASCENDING, UpdateOne
-from pymongo.cursor import Cursor
-from pymongo.results import BulkWriteResult
+from pymongo import MongoClient
 from pymongo.database import Database
 
+from geodata.csc.urls import UrlsCSC
+from geodata.csc.downloads import download_csv
 from geodata.db.colls.countries import CountriesColl
 from geodata.db.colls.states import StatesColl
 from geodata.db.colls.cities import CitiesColl
@@ -49,44 +49,25 @@ class WorldDataDB(BaseWorldDataDB):
     def __init__(self, mongo_client: MongoClient, db_name: str = WORLD_DATA):
         super().__init__(mongo_client=mongo_client, db_name=db_name)
 
-    # def set_unique_keys(self) -> None:
-    #     for symbol, interval in iter_all_symbol_interval():
-    #         coll_name = get_collection_name(symbol, interval)
-    #         collection = self.get_collection(symbol, interval)
-    #         collection.collection.create_index([(TIME_OPEN, ASCENDING)], unique=True)
-    #         print(f"Creación de índice para: {coll_name}")
+    def set_unique_keys(self) -> None:
+        self.countries.coll.create_index([(self.countries.column_id_csc, 1)], unique=True)
+        self.states.coll.create_index([(self.states.column_id_csc, 1)], unique=True)
+        self.cities.coll.create_index([(self.cities.column_id_csc, 1)], unique=True)
 
+    def download_csc(self, verbose: bool = True) -> None:
+        print("~"*40)
+        print(f"{'countries':~^40}")
+        df_countries = download_csv(url=UrlsCSC.countries)
+        self.countries.process_df_csc(df_countries, verbose=verbose)
 
+        print("~"*40)
+        print("~"*40)
+        print(f"{'states':~^40}")
+        df_states = download_csv(url=UrlsCSC.states)
+        self.states.process_df_csc(df_states, verbose=verbose)
 
-
-# def filter_time_open(time_open: int) -> dict:
-#     return {TIME_OPEN: time_open}
-# 
-# def kline_to_update_one(kline: KLine) -> UpdateOne:
-#     """ Transforma el KLine en un UpdateOne para la subida a la DB."""
-#     return UpdateOne(
-#         filter_time_open(kline.time_open),           # Criterio de búsqueda por time_open
-#         {'$setOnInsert': kline.model_dump()},   # $setOnInsert solo actualiza en caso de insertar
-#         upsert = True                           # Realiza un insert si el documento no existe.
-#     )
-
-# class CollectionKLines:
-#     def __init__(self, db: Database, coll_name: str):
-#         self._collection = db[coll_name]
-#         self._coll_name = coll_name
-#     
-#     @property
-#     def collection(self) -> Collection:
-#         return self._collection
-#     
-#     @property
-#     def coll_name(self) -> str:
-#         return self._coll_name
-#     
-#     def insert_klines(self, klines: List[KLine]) -> BulkWriteResult:
-#         operations = [kline_to_update_one(kline) for kline in klines]
-#         result = self.collection.bulk_write(operations)
-#         return result
-# 
-#     def find_klines(self, direction=ASCENDING) -> Cursor:
-#         return self.collection.find().sort(TIME_OPEN, direction)
+        print("~"*40)
+        print("~"*40)
+        print(f"{'cities':~^40}")
+        df_cities = download_csv(url=UrlsCSC.cities)
+        self.cities.process_df_csc(df_cities, verbose=verbose)
