c2b7f0a1774448b2da77acc9b9a16c7da9da4e31
AlejoPrietoDavalos <alejoprietodavalos@gmail.com>
2024-03-10 16:45:48 -0300

    querys into a functions, and search_id_wikidata is abstracted

--- geodata/wikidata/search.py
+++ geodata/wikidata/search.py
@@ -1,89 +1,85 @@
 from typing import Tuple, List
 
-from SPARQLWrapper import SPARQLWrapper, JSON
+from SPARQLWrapper import JSON
 
+from geodata.wikidata.sparql import get_sparql
+from geodata.wikidata._etc import _raise_model_error
 from geodata.db.models.country import Country
 from geodata.db.models.state import State
 from geodata.db.models.city import City
 
-def url_wikidata_sparql() -> str:
-    return "https://query.wikidata.org/sparql"
 
-def get_sparql() -> SPARQLWrapper:
-    return SPARQLWrapper(url_wikidata_sparql())
+def query_country_wikidata_id(country: Country) -> str:
+    return f"""
+        SELECT ?country ?countryLabel WHERE {{
+            ?country wdt:P31 wd:Q6256;
+                    wdt:P297 "{country.country_code}";
+            SERVICE wikibase:label {{ bd:serviceParam wikibase:language "[AUTO_LANGUAGE],en". }}
+        }}
+        LIMIT 1
+        """
+
+def query_state_wikidata_id(state: State) -> str:
+    return f"""
+        SELECT ?place ?placeLabel WHERE {{
+            ?place wdt:P31/wdt:P279* wd:Q10864048;
+                rdfs:label "{state.state_name}"@en;
+                wdt:P17 ?country. # Country of the place.
+            ?country wdt:P297 "{state.country_code}".
+            SERVICE wikibase:label {{ bd:serviceParam wikibase:language "[AUTO_LANGUAGE],en". }}
+        }}
+        LIMIT 1
+        """
+
+def query_city_wikidata_id(city: City) -> str:
+    return f"""
+        SELECT ?place ?placeLabel WHERE {{
+            ?place wdt:P31/wdt:P279* wd:Q486972;
+                rdfs:label "{city.city_name}"@en;
+                wdt:P17 ?country. # País del lugar
+            ?country wdt:P297 "{city.country_code}".
+            SERVICE wikibase:label {{ bd:serviceParam wikibase:language "[AUTO_LANGUAGE],en". }}
+        }}
+        LIMIT 1
+        """
 
 
-def search_country_wikidata_id(country: Country) -> Tuple[int, str | None]:
-    """
-    Function to find the Wikidata ID of a country given its name and country code.
-    :return: id_wikidata of the country if found, None otherwise.
-    """
+def query_from_model(model: Country | State | City) -> str:
+    if isinstance(model, Country):
+        query = query_country_wikidata_id(model)
+    elif isinstance(model, State):
+        query = query_state_wikidata_id(model)
+    elif isinstance(model, City):
+        query = query_city_wikidata_id(model)
+    else:
+        _raise_model_error()
+    return query
+
+def _get_model_name(model: Country | State | City) -> str:
+    """ The API brings the field `country` for countries, and `place` for what we call `state` and `city`."""
+    if isinstance(model, Country):
+        _model_name = "country"
+    elif isinstance(model, State) or isinstance(model, State):
+        _model_name = "place"
+    else:
+        _raise_model_error()
+    return _model_name
+
+def _id_wikidata_from_results(results, model: Country | State | City) -> str | None:
+    bindings = results["results"]["bindings"]
+    MODEL_NAME = _get_model_name(model)
+    id_wikidata = None if len(bindings) == 0 else bindings[0][MODEL_NAME]["value"].split('/')[-1]
+    return id_wikidata
+
+def search_id_wikidata(model: Country | State | City) -> Tuple[int, str | None]:
+    query = query_from_model(model)
     sparql = get_sparql()
-    query = f"""
-    SELECT ?country ?countryLabel WHERE {{
-        ?country wdt:P31 wd:Q6256; # Instance of country.
-                wdt:P297 "{country.country_code}"; # Country code ISO 3166-1 alpha-2.
-        SERVICE wikibase:label {{ bd:serviceParam wikibase:language "[AUTO_LANGUAGE],en". }}
-    }}
-    LIMIT 1
-    """
     sparql.setQuery(query)
     sparql.setReturnFormat(JSON)
+
     results = sparql.query().convert()
-
-    bindings = results["results"]["bindings"]
-    country_id_wikidata = None if len(bindings) == 0 else bindings[0]["country"]["value"].split('/')[-1]
-    return country.country_id_csc, country_id_wikidata
-
-
-def search_state_wikidata_id(state: State) -> Tuple[int, str | None]:
-    """
-    Function to find the Wikidata ID of a state given its name and country code.
-    :return: id_wikidata of the state if found, None otherwise.
-    """
-    sparql = get_sparql()
-    query = f"""
-    SELECT ?place ?placeLabel WHERE {{
-        ?place wdt:P31/wdt:P279* wd:Q10864048; # Instance or Subclass of administrative territorial entity.
-            rdfs:label "{state.state_name}"@en;
-            wdt:P17 ?country. # Country of the place.
-        ?country wdt:P297 "{state.country_code}". # Country code ISO 3166-1 alpha-2.
-        SERVICE wikibase:label {{ bd:serviceParam wikibase:language "[AUTO_LANGUAGE],en". }}
-    }}
-    LIMIT 1
-    """
-    sparql.setQuery(query)
-    sparql.setReturnFormat(JSON)
-    results = sparql.query().convert()
-
-    bindings = results["results"]["bindings"]
-    state_id_wikidata = None if len(bindings) == 0 else bindings[0]["place"]["value"].split('/')[-1]
-    return state.state_id_csc, state_id_wikidata
-
-
-def search_city_wikidata_id(city: City) -> Tuple[int, str | None]:
-    """
-    Function to find the Wikidata ID of a place given its name and country code.
-    :return: id_wikidata of the place if found, None otherwise.
-    """
-    sparql = get_sparql()
-    query = f"""
-    SELECT ?place ?placeLabel WHERE {{
-        ?place wdt:P31/wdt:P279* wd:Q486972; # Instance or Subclass of human as settlement.
-            rdfs:label "{city.city_name}"@en;
-            wdt:P17 ?country. # País del lugar
-        ?country wdt:P297 "{city.country_code}". # Country code ISO 3166-1 alpha-2.
-        SERVICE wikibase:label {{ bd:serviceParam wikibase:language "[AUTO_LANGUAGE],en". }}
-    }}
-    LIMIT 1
-    """
-    sparql.setQuery(query)
-    sparql.setReturnFormat(JSON)
-    results = sparql.query().convert()
-
-    bindings = results["results"]["bindings"]
-    city_id_wikidata = None if len(bindings) == 0 else bindings[0]["place"]["value"].split('/')[-1]
-    return city.city_id_csc, city_id_wikidata
+    id_wikidata = _id_wikidata_from_results(results=results, model=model)
+    return model.id_csc, id_wikidata
 
 
 def search_websites_and_postal_codes(id_wikidata: str | None) -> Tuple[List[str], List[str]]:
