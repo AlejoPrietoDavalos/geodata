6f1347829eee56f93ff8af91bee77d495f61b8be
AlejoPrietoDavalos <alejoprietodavalos@gmail.com>
2024-04-07 23:48:02 -0300

    .

---
+++ download_postals_wikipedia.py
@@ -0,0 +1,10 @@
+from geodata.db.client import WorldDataDB
+
+def main(max_workers: int = 5, verbose: bool = True):
+    db = WorldDataDB()
+    db.download_postals_wikipedia(max_workers=max_workers, verbose=verbose)
+
+if __name__ == "__main__":
+    MAX_WORKERS = 5
+    VERBOSE = True      # Can redirect to .log file.
+    main(max_workers=MAX_WORKERS, verbose=VERBOSE)
